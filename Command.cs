cee6b21ab3828488c0997cc6b1eb6e8d03de34f6
Egorov <mymailegorov@gmail.com>
2018-12-17 21:26:57 +0300

    Start

---
+++ Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
+[TransactionAttribute(TransactionMode.Manual)]
+[RegenerationAttribute(RegenerationOption.Manual)]
+
+public class Command : IExternalCommand
+{
+	public Result Execute(ExternalCommandData commandData,
+		ref string messege,
+		ElementSet elements)
+	{
+		UIApplication uiApp = commandData.Application;
+		Document doc = uiApp.ActiveUIDocument.Document;
+
+		WorkFile file = new WorkFile();
+		file.ShowFileDialog();
+		//string st = file.Path;
+		TaskDialog.Show("show", (file.Path).ToString());	
+
+		return Result.Succeeded;
+	}
+}
---
+++ WorkFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
+public class WorkFile
+{
+	OpenFileDialog ofd = new OpenFileDialog();
+
+	//string path = null;
+	public OpenFileDialog ShowFileDialog()
+	{
+		ofd.ShowDialog();
+		return ofd;
+	}
+	
+	public string Path
+	{
+		get
+		{			
+			return ShowFileDialog().FileName;
+		}
+	}		
+}
